e04b62c762d2fe5113839599ef9e1b2ba9d5164d
stefan-popovic <60715574+stefan-popovic@users.noreply.github.com>
2021-01-10 23:22:57 +0100

    End of section 9

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -11,6 +11,8 @@ namespace API.DTOs
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,9 @@ export interface User {
     username: string;
     token: string;
 }
+
+console.log(vardata);
+var vardata;
+
+let letdata = 5;
+console.log(letdata);
